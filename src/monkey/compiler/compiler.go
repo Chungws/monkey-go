a6d7af7f011cd13b8a1c7bc3c77a0098e965d6dd
WooSeong Chung <jungyh0528@gmail.com>
2024-04-26 00:06:53 +0900

    feat: check num parameter

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -210,6 +210,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.FunctionLiteral:
 		c.enterScope()
 
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body)
 		if err != nil {
 			return err
@@ -225,7 +229,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 		numLocals := c.symbolTable.numDefinitions
 		instructions := c.leaveScope()
-		compiled := &object.CompiledFunction{Instructions: instructions, NumLocals: numLocals}
+		compiled := &object.CompiledFunction{
+			Instructions:  instructions,
+			NumLocals:     numLocals,
+			NumParameters: len(node.Parameters),
+		}
 		c.emit(code.OpConstant, c.addConstant(compiled))
 
 	case *ast.CallExpression:
